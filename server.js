6805347e62577b24f3df52606c96303dc90d2d48
jossif eleftteriadis <jossif.elefteriadis@appliedtechnology.se>
2019-10-29 15:22:19 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+// const fetch = require('node-fetch');
+
+const url = 
+
+
+// fetch()
+
+app.listen(port, () => console.log('Server listening on port ' + port));
