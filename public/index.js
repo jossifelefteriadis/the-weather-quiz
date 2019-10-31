314a6570ce478aec054f819df87e630dc2d2aa8e
jossif eleftteriadis <jossif.elefteriadis@appliedtechnology.se>
2019-10-31 16:10:01 +0100

    Add final logic and css

--- public/index.js
+++ public/index.js
@@ -1,16 +1,19 @@
+const cities = [
+    'Barcelona', 'Athens', 'Stockholm', 'Uppsala', 'Kiev', 'Berlin', 'Rome', 'London', 'Paris', 'Amsterdam',
+    'Prague', 'Madrid', 'Lisbon', 'Vienna', 'Copenhagen', 'Budapest', 'Dublin', 'Munich', 'Venice', 'Milan',
+    'Istanbul', 'Hamburg', 'Porto', 'Helsinki', 'Oslo', 'Zagreb', 'Riga', 'Lyon', 'Nice', 'Belgrade'
+]
 
-// const randomCity = document.querySelector('.btn__generate');
-const question = document.querySelector('.question');
+const randomCity = document.querySelector('.container__btn__generate');
+const question = document.querySelector('.container__question');
 const fourAnswers = document.querySelector('.answer_buttons');
 let shuffledCity;
 let shuffledAnswers;
-const counter = document.querySelector('.counter');
-let count = 0;
 
 function generateCity() {
+    randomCity.innerText = 'NEXT';
     shuffledCity = cities[Math.floor(Math.random() * cities.length)];
     getCityWeather(shuffledCity);
-    console.log(count);
 };
 
 const getCityWeather = function (shuffledCity) {
@@ -37,7 +40,6 @@ function generateCities(correctCity) {
             answers.push(city);
         }
     }
-    console.log('before shuffle', answers);
     shuffleAnswers(answers, correctCity);
 }
 
@@ -47,7 +49,6 @@ function shuffleAnswers(answers, correctCity) {
         const j = Math.floor(Math.random() * (i + 1));
         [answers[i], answers[j]] = [answers[j], answers[i]];
     }
-    // shuffledAnswers = answers[Math.floor(Math.random() * answers.length)];
     showAnswers(answers, correctCity);
 }
 
@@ -66,51 +67,12 @@ function showAnswers(answers, correctCity) {
         const selected = event.target;
         if (selected.classList.contains('right')) {
             selected.classList.add('selectedRight')
-            console.log(count);
-            count += 1;
-            counter.innerHTML = `Your count is: ${count}`;
-            console.log('Dude, good job!')
         } if (!selected.classList.contains('right'))  {
             selected.classList.add('selectedWrong');
-            console.log('Hah, you suck! The right answer was', correctCity);
         }
         const rightA = document.querySelector('.right');
         rightA.classList.add('selectedRight');
-        selected.classList.remove('right');
-        fourAnswers.map(disable => {
-            disable = document.querySelector('.btn').disabled = true
-        })
-        //map through för att disable alla
-        // document.querySelector('.btn').disabled = true;
-        // fourAnswers.removeEventListener('click', showAnswers, false);
     })
-    
-    // const buttons = document.querySelector('button');
-    // buttons.forEach(button => {
-    //     button.addEventListener('click', () => {
-    //         if (button === correctCity) {
-    //         }
-    //     })
-    // })
-    // buttons.addEventListener('click', () => {
-    //     if (button === correctCity) {
-    //         console.log('Dude, good job!')
-    //     }
-    //     console.log('Hah, you suck! The right answer was', correctCity);
-    // })
-    // selectAnswer(correctCity);
 }
 
-// function selectAnswer(correctCity) {
-// }
-
-// randomCity.addEventListener('click', generateCity)
-
-
-const cities = [
-    'Barcelona', 'Athens', 'Stockholm', 'Uppsala', 'Kiev', 'Berlin', 'Rome', 'London', 'Paris', 'Amsterdam',
-    'Prague', 'Madrid', 'Lisbon', 'Vienna', 'Copenhagen', 'Budapest', 'Dublin', 'Munich', 'Venice', 'Milan',
-    'Istanbul', 'Hamburg', 'Porto', 'Helsinki', 'Oslo', 'Zagreb', 'Riga', 'Lyon', 'Nice', 'Belgrade'
-]
-
-window.addEventListener('DOMContentLoaded', generateCity)
+randomCity.addEventListener('click', generateCity);
